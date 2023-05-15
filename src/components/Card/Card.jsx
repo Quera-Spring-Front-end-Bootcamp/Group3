4b16d5de95a9528a6ea8f5cf0600477f4eddba70
Mehdi RahimSirat <mahdi.r803@yahoo.com>
2023-05-15 22:46:37 +0330

    add children props for Card component

--- src/components/Card/Card.jsx
+++ src/components/Card/Card.jsx
@@ -2,7 +2,7 @@
 export const Card = (props) => {
     return (
         <div className="flex w-5/12 flex-col items-center absolute rounded-[20px] shadow-1 p-6">
-            
+            {props.children}
             
         </div>
     );
