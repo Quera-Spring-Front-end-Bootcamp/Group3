9a51eeaaf34f48da09f91c467945ab4734570d79
Rosehajizadeh <rose_kr_2020@yahoo.com>
2023-05-18 17:36:31 +0330

    auth card component finished

--- src/components/Card/AuthCard.jsx
+++ src/components/Card/AuthCard.jsx
@@ -1,9 +1,11 @@
 const AuthCard = (props) => {
   return (
     <div
-      className={`flex flex-col items-center shadow-md bg-white p-6 rounded-[20px] ${props.className}`}
+      className={`flex flex-col items-center shadow-[0_12px_50px_-15px_rgba(0,0,0,0.18)] bg-white p-6 rounded-[20px] ${props.className}`}
     >
-      <h2 className="font-bold">{props.title}</h2>
+      <p className="font-semibold text-[32px]/[50px] text-right">
+        {props.title}
+      </p>
       {props.children}
     </div>
   );
--- src/pages/auth/login.jsx
+++ src/pages/auth/login.jsx
@@ -1,11 +1,4 @@
-import AuthCard from "../../components/Card/AuthCard";
-
 const Login = () => {
-  return (
-    <>
-      <h1>Login Page</h1>
-      <AuthCard title="به کوئرا تسک منیجر خوش برگشتی :)"></AuthCard>
-    </>
-  );
+  return <h1>Login Page</h1>;
 };
 export default Login;
