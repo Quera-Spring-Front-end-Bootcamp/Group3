1639098c3d0360d06faf306dac6f4f510d403b57
Mehdi RahimSirat <mahdi.r803@yahoo.com>
2023-05-18 09:37:34 +0330

    fix class names

--- src/components/Card/Card.jsx
+++ src/components/Card/Card.jsx
@@ -1,7 +1,7 @@
 
 export const Card = (props) => {
     return (
-        <div className={`flex w-5/12 flex-col items-center rounded-[20px] shadow-1 p-6 ${props.className}`}>
+        <div className={`flex flex-col items-center shadow-1 p-6 ${props.className}`}>
             {props.children}
             
         </div>
