3c67123a5d856bea51b3c9e70f0493a1b7d259a4
Rosehajizadeh <rose_kr_2020@yahoo.com>
2023-05-18 19:02:41 +0330

    fix some changes

--- src/components/Button.jsx
+++ src/components/Button.jsx
@@ -1,12 +1,13 @@
 // import React from "react";
 
-function Button(props) {
+function Button({ title, handleClick, ...props }) {
   return (
     <button
-      onClick={props.handleClick}
+      onClick={handleClick}
       className="rounded-md	bg-secondary p-2.5 font-bold text-white text-right text-sm/[22px]"
+      {...props}
     >
-      {props.title}
+      {title}
     </button>
   );
 }
