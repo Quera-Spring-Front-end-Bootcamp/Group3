b217a14602562635b20b75a413379c72631bb6c7
Rosehajizadeh <rose_kr_2020@yahoo.com>
2023-05-18 17:22:54 +0330

    button component finished

--- src/components/Button.jsx
+++ src/components/Button.jsx
@@ -1,7 +1,11 @@
 // import React from "react";
 
-function Button() {
-  return <button>hi</button>;
+function Button(props) {
+  return (
+    <button className="rounded-md	bg-secondary p-2.5 font-bold text-white  text-right text-sm/[22px]">
+      {props.title}
+    </button>
+  );
 }
 
 export default Button;
