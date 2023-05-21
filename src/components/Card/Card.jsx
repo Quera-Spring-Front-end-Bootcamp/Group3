7b477f2cdb1f9a86a0f9494f1c3d11987e349bd2
nima doustdar <nimadoustdar99@gmail.com>
2023-05-21 13:40:22 +0330

    modify card comp

--- src/components/Card/Card.jsx
+++ src/components/Card/Card.jsx
@@ -1,7 +1,9 @@
 const Card = ({ title, children, className }) => {
   return (
-    <div className={`flex flex-col items-center bg-white ${className}`}>
-      <p className="font-semibold text-[32px]/[50px] text-right">{title}</p>
+    <div className={`flex flex-col  bg-white ${className}`}>
+      {title && (
+        <p className="font-semibold text-[32px]/[50px] text-right">{title}</p>
+      )}
       {children}
     </div>
   );
@@ -9,7 +11,7 @@ const Card = ({ title, children, className }) => {
 
 Card.defaultProps = {
   title: "عنوان",
-  className: "shadow-[0_12px_50px_-15px_rgba(0,0,0,0.18)] p-6 rounded-[20px]",
+  className: "shadow-[0_12px_50px_-15px_rgba(0,0,0,0.18)] p-6 rounded-[20px] items-center",
 };
 
 export default Card;
