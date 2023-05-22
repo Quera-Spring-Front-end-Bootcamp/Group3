bfba364c3c6fb0724af3f5cfd09be30caa69eebe
Mehdi RahimSirat <mahdi.r803@yahoo.com>
2023-05-23 01:02:57 +0330

    reorder icon

--- src/components/Icons.jsx
+++ src/components/Icons.jsx
@@ -50,113 +50,157 @@ const icons = {
   ),
   TaskIcon: (
     <svg
-              width="14"
-              height="14"
-              viewBox="0 0 14 14"
-              fill="none"
-              xmlns="http://www.w3.org/2000/svg"
-            >
-              <path
-                d="M2.99902 2.99913H11.0024"
-                stroke="#BDC0C6"
-                stroke-width="0.75"
-                stroke-linecap="round"
-                stroke-linejoin="round"
-              />
-              <path
-                d="M4.49902 5.0001H11.0017"
-                stroke="#BDC0C6"
-                stroke-width="0.75"
-                stroke-linecap="round"
-                stroke-linejoin="round"
-              />
-              <path
-                d="M4.49902 7.0001H11.0017"
-                stroke="#BDC0C6"
-                stroke-width="0.75"
-                stroke-linecap="round"
-                stroke-linejoin="round"
-              />
-              <path
-                d="M2.99902 9.00206H11.0024"
-                stroke="#BDC0C6"
-                stroke-width="0.75"
-                stroke-linecap="round"
-                stroke-linejoin="round"
-              />
-              <path
-                d="M7 11.0021H11.0017"
-                stroke="#BDC0C6"
-                stroke-width="0.75"
-                stroke-linecap="round"
-                stroke-linejoin="round"
-              />
-            </svg>
+      width="14"
+      height="14"
+      viewBox="0 0 14 14"
+      fill="none"
+      xmlns="http://www.w3.org/2000/svg"
+    >
+      <path
+        d="M2.99902 2.99913H11.0024"
+        stroke="#BDC0C6"
+        stroke-width="0.75"
+        stroke-linecap="round"
+        stroke-linejoin="round"
+      />
+      <path
+        d="M4.49902 5.0001H11.0017"
+        stroke="#BDC0C6"
+        stroke-width="0.75"
+        stroke-linecap="round"
+        stroke-linejoin="round"
+      />
+      <path
+        d="M4.49902 7.0001H11.0017"
+        stroke="#BDC0C6"
+        stroke-width="0.75"
+        stroke-linecap="round"
+        stroke-linejoin="round"
+      />
+      <path
+        d="M2.99902 9.00206H11.0024"
+        stroke="#BDC0C6"
+        stroke-width="0.75"
+        stroke-linecap="round"
+        stroke-linejoin="round"
+      />
+      <path
+        d="M7 11.0021H11.0017"
+        stroke="#BDC0C6"
+        stroke-width="0.75"
+        stroke-linecap="round"
+        stroke-linejoin="round"
+      />
+    </svg>
   ),
-  CalendarIcon: (<svg
-    width="16"
-    height="16"
-    viewBox="0 0 16 16"
-    fill="none"
-    xmlns="http://www.w3.org/2000/svg"
-  >
-    <path
-      d="M3.33333 14.0001V2.62012"
-      stroke="#FB0606"
-      stroke-linecap="round"
-      stroke-linejoin="round"
-    />
-    <path
-      d="M12.6663 9.34699V2.66699"
-      stroke="#FB0606"
-      stroke-linecap="round"
-      stroke-linejoin="round"
-    />
-    <path
-      d="M3.33301 9.3333C3.33301 9.3333 3.91634 8.84863 5.66634 8.84863C7.41634 8.84863 8.58301 9.99997 10.333 9.99997C12.083 9.99997 12.6663 9.34863 12.6663 9.34863"
-      stroke="#FB0606"
-      stroke-linecap="round"
-      stroke-linejoin="round"
-    />
-    <path
-      d="M3.33301 2.62133C3.33301 2.62133 3.91634 2 5.66634 2C7.41634 2 8.58301 3.15133 10.333 3.15133C12.083 3.15133 12.6663 2.66667 12.6663 2.66667"
-      stroke="#FB0606"
-      stroke-linecap="round"
-      stroke-linejoin="round"
-    />
-  </svg>),
-  TimeIcon: (<svg
-    width="18"
-    height="18"
-    viewBox="0 0 18 18"
-    fill="none"
-    xmlns="http://www.w3.org/2000/svg"
-  >
-    <path
-      d="M11.1365 7.74121L8.46711 10.409L6.8623 8.80899"
-      stroke="#BDC0C6"
-      stroke-linecap="round"
-      stroke-linejoin="round"
-    />
-    <rect
-      x="2.99707"
-      y="2.99707"
-      width="12.005"
-      height="12.005"
-      rx="3.33333"
-      stroke="#BDC0C6"
-      stroke-linecap="round"
-      stroke-linejoin="round"
-    />
-  </svg>),
-  AcceptIcon:(<svg width="20" height="20" viewBox="0 0 20 20" fill="none" xmlns="http://www.w3.org/2000/svg">
-  <path d="M6.85667 9.35167L10.0042 12.5L16.4 6.10417C15.0833 3.94583 12.7133 2.5 10 2.5C5.8575 2.5 2.5 5.8575 2.5 10C2.5 14.1425 5.8575 17.5 10 17.5C13.86 17.5 17.0358 14.5833 17.4508 10.8333" stroke="#323232" stroke-width="1.25" stroke-linecap="round" stroke-linejoin="round"/>
-  </svg>),
-  MoreIcon:( <svg width="22" height="22" viewBox="0 0 22 22" fill="none" xmlns="http://www.w3.org/2000/svg">
-  <path d="M16.4186 10.9998C16.4186 11.2301 16.232 11.4167 16.0018 11.4167C15.7716 11.4167 15.585 11.2301 15.585 10.9998C15.585 10.7696 15.7716 10.583 16.0018 10.583C16.232 10.583 16.4186 10.7696 16.4186 10.9998" stroke="#323232" stroke-width="1.25" stroke-linecap="round" stroke-linejoin="round"/>
-  <path d="M11.4167 10.9998C11.4167 11.2301 11.2301 11.4167 10.9998 11.4167C10.7696 11.4167 10.583 11.2301 10.583 10.9998C10.583 10.7696 10.7696 10.583 10.9998 10.583C11.2301 10.583 11.4167 10.7696 11.4167 10.9998" stroke="#323232" stroke-width="1.25" stroke-linecap="round" stroke-linejoin="round"/>
-  <path d="M6.41474 10.9998C6.41474 11.2301 6.22811 11.4167 5.99789 11.4167C5.76768 11.4167 5.58105 11.2301 5.58105 10.9998C5.58105 10.7696 5.76768 10.583 5.99789 10.583C6.22811 10.583 6.41474 10.7696 6.41474 10.9998" stroke="#323232" stroke-width="1.25" stroke-linecap="round" stroke-linejoin="round"/>
-  </svg>),
+  CalendarIcon: (
+    <svg
+      width="16"
+      height="16"
+      viewBox="0 0 16 16"
+      fill="none"
+      xmlns="http://www.w3.org/2000/svg"
+    >
+      <path
+        d="M3.33333 14.0001V2.62012"
+        stroke="#FB0606"
+        stroke-linecap="round"
+        stroke-linejoin="round"
+      />
+      <path
+        d="M12.6663 9.34699V2.66699"
+        stroke="#FB0606"
+        stroke-linecap="round"
+        stroke-linejoin="round"
+      />
+      <path
+        d="M3.33301 9.3333C3.33301 9.3333 3.91634 8.84863 5.66634 8.84863C7.41634 8.84863 8.58301 9.99997 10.333 9.99997C12.083 9.99997 12.6663 9.34863 12.6663 9.34863"
+        stroke="#FB0606"
+        stroke-linecap="round"
+        stroke-linejoin="round"
+      />
+      <path
+        d="M3.33301 2.62133C3.33301 2.62133 3.91634 2 5.66634 2C7.41634 2 8.58301 3.15133 10.333 3.15133C12.083 3.15133 12.6663 2.66667 12.6663 2.66667"
+        stroke="#FB0606"
+        stroke-linecap="round"
+        stroke-linejoin="round"
+      />
+    </svg>
+  ),
+  TimeIcon: (
+    <svg
+      width="18"
+      height="18"
+      viewBox="0 0 18 18"
+      fill="none"
+      xmlns="http://www.w3.org/2000/svg"
+    >
+      <path
+        d="M11.1365 7.74121L8.46711 10.409L6.8623 8.80899"
+        stroke="#BDC0C6"
+        stroke-linecap="round"
+        stroke-linejoin="round"
+      />
+      <rect
+        x="2.99707"
+        y="2.99707"
+        width="12.005"
+        height="12.005"
+        rx="3.33333"
+        stroke="#BDC0C6"
+        stroke-linecap="round"
+        stroke-linejoin="round"
+      />
+    </svg>
+  ),
+  AcceptIcon: (
+    <svg
+      width="20"
+      height="20"
+      viewBox="0 0 20 20"
+      fill="none"
+      xmlns="http://www.w3.org/2000/svg"
+    >
+      <path
+        d="M6.85667 9.35167L10.0042 12.5L16.4 6.10417C15.0833 3.94583 12.7133 2.5 10 2.5C5.8575 2.5 2.5 5.8575 2.5 10C2.5 14.1425 5.8575 17.5 10 17.5C13.86 17.5 17.0358 14.5833 17.4508 10.8333"
+        stroke="#323232"
+        stroke-width="1.25"
+        stroke-linecap="round"
+        stroke-linejoin="round"
+      />
+    </svg>
+  ),
+  MoreIcon: (
+    <svg
+      width="22"
+      height="22"
+      viewBox="0 0 22 22"
+      fill="none"
+      xmlns="http://www.w3.org/2000/svg"
+    >
+      <path
+        d="M16.4186 10.9998C16.4186 11.2301 16.232 11.4167 16.0018 11.4167C15.7716 11.4167 15.585 11.2301 15.585 10.9998C15.585 10.7696 15.7716 10.583 16.0018 10.583C16.232 10.583 16.4186 10.7696 16.4186 10.9998"
+        stroke="#323232"
+        stroke-width="1.25"
+        stroke-linecap="round"
+        stroke-linejoin="round"
+      />
+      <path
+        d="M11.4167 10.9998C11.4167 11.2301 11.2301 11.4167 10.9998 11.4167C10.7696 11.4167 10.583 11.2301 10.583 10.9998C10.583 10.7696 10.7696 10.583 10.9998 10.583C11.2301 10.583 11.4167 10.7696 11.4167 10.9998"
+        stroke="#323232"
+        stroke-width="1.25"
+        stroke-linecap="round"
+        stroke-linejoin="round"
+      />
+      <path
+        d="M6.41474 10.9998C6.41474 11.2301 6.22811 11.4167 5.99789 11.4167C5.76768 11.4167 5.58105 11.2301 5.58105 10.9998C5.58105 10.7696 5.76768 10.583 5.99789 10.583C6.22811 10.583 6.41474 10.7696 6.41474 10.9998"
+        stroke="#323232"
+        stroke-width="1.25"
+        stroke-linecap="round"
+        stroke-linejoin="round"
+      />
+    </svg>
+  ),
 };
 
 export default icons;
