c76c7e99d6d68b897be317af5d32b3f0bb274236
nima doustdar <nimadoustdar99@gmail.com>
2023-05-23 11:19:02 +0330

    add icon plus to boardTitle

--- src/assets/Icons.jsx
+++ src/assets/Icons.jsx
@@ -48,6 +48,61 @@ const icons = {
       />
     </svg>
   ),
+  DotsMenu: (
+    <svg
+      width="20"
+      height="21"
+      viewBox="0 0 20 21"
+      fill="none"
+      xmlns="http://www.w3.org/2000/svg"
+    >
+      <path
+        d="M15.4196 10.5008C15.4196 10.731 15.233 10.9177 15.0028 10.9177C14.7726 10.9177 14.5859 10.731 14.5859 10.5008C14.5859 10.2706 14.7726 10.084 15.0028 10.084C15.233 10.084 15.4196 10.2706 15.4196 10.5008"
+        stroke="#323232"
+        strokeWidth="1.25"
+        strokeLinecap="round"
+        strokeLinejoin="round"
+      />
+      <path
+        d="M10.4167 10.5008C10.4167 10.731 10.2301 10.9177 9.99985 10.9177C9.76963 10.9177 9.58301 10.731 9.58301 10.5008C9.58301 10.2706 9.76963 10.084 9.99985 10.084C10.2301 10.084 10.4167 10.2706 10.4167 10.5008"
+        stroke="#323232"
+        strokeWidth="1.25"
+        strokeLinecap="round"
+        strokeLinejoin="round"
+      />
+      <path
+        d="M5.41474 10.5008C5.41474 10.731 5.22811 10.9177 4.99789 10.9177C4.76768 10.9177 4.58105 10.731 4.58105 10.5008C4.58105 10.2706 4.76768 10.084 4.99789 10.084C5.22811 10.084 5.41474 10.2706 5.41474 10.5008"
+        stroke="#323232"
+        strokeWidth="1.25"
+        strokeLinecap="round"
+        strokeLinejoin="round"
+      />
+    </svg>
+  ),
+  Add: (
+    <svg
+      width="24"
+      height="25"
+      viewBox="0 0 24 25"
+      fill="none"
+      xmlns="http://www.w3.org/2000/svg"
+    >
+      <path
+        d="M12.25 7.25V17.25"
+        stroke="#323232"
+        strokeWidth="1.5"
+        strokeLinecap="round"
+        strokeLinejoin="round"
+      />
+      <path
+        d="M17.25 12.25H7.25"
+        stroke="#323232"
+        strokeWidth="1.5"
+        strokeLinecap="round"
+        strokeLinejoin="round"
+      />
+    </svg>
+  ),
 };
 
 export default icons;
--- src/components/BoardTitle/BoardTitle.jsx
+++ src/components/BoardTitle/BoardTitle.jsx
@@ -1,24 +1,53 @@
-export const BoardTitle = ({ badgeValue }) => {
+import { useState } from "react";
+import icons from "../../assets/Icons";
+
+export const BoardTitle = ({ badgeValue, clickAddHandler }) => {
   const card = [
     { id: "1", title: "Open", color: "#F98F2E" },
     { id: "2", title: "In progress", color: "#2E7FF9" },
     { id: "3", title: "Pending", color: "#DEC908" },
     { id: "4", title: "To Do", color: "#F98F2E" },
   ];
+  const [hoverTooltip, setHoverTooltip] = useState(false);
+  const handleMouseEnter=()=>{
+    setHoverTooltip(true)
+  }
+  const handleMouseLeave=()=>{
+    setHoverTooltip(false )
+
+  }
   return (
     <div className="flex gap-5 mt-5">
       {card.map((item) => (
         <div
           key={item.id}
-          className={`max-w-[250px] w-full px-4 py-2 flex gap-1 items-center  shadow-[0_2px_8px_rgba(0,0,0,0.18)] rounded border-t `}
+          className={`group transition-all  max-w-[250px] w-full px-4 py-2 flex  items-center justify-between shadow-[0_2px_8px_rgba(0,0,0,0.18)] rounded border-t group-hover:opacity-100`}
           style={{ borderTopColor: item.color }}
         >
-          <span className="text-[#1E1E1E] text-base font-medium">
-            {item.title}
-          </span>
-          <span className="text-[10px] p-1  grid items-center bg-[#F4F4F4] text-[#1E1E1E] font-medium rounded-full">
-            {badgeValue}
-          </span>
+          <div className="flex gap-1 items-center">
+            <span className=" text-[#1E1E1E] text-base font-medium">
+              {item.title}
+            </span>
+            <span className="text-[10px] p-1  grid items-center bg-[#F4F4F4] text-[#1E1E1E] font-medium rounded-full">
+              {badgeValue}
+            </span>
+          </div>
+          <div className="flex items-center gap-1 opacity-0 transition-opacity group-hover:opacity-100 ">
+            <div className="cursor-pointer">{icons.DotsMenu}</div>
+            <div
+              className="cursor-pointer group relative"
+              onClick={clickAddHandler}
+              onMouseEnter={handleMouseEnter}
+              onMouseLeave={handleMouseLeave}
+            >
+              {hoverTooltip && (
+                <span className="absolute flex   left-1/2  -top-2 -translate-x-1/2 -translate-y-full  w-max px-2 py-1 bg-[#3A4046] rounded-lg text-center text-white text-xs after:content-[''] after:absolute after:left-1/2 after:top-[100%] after:-translate-x-1/2 after:border-8 after:border-x-transparent after:border-b-transparent after:border-[#3A4046]">
+                  افزودن تسک
+                </span>
+              )}
+              {icons.Add}
+            </div>
+          </div>
         </div>
       ))}
     </div>
