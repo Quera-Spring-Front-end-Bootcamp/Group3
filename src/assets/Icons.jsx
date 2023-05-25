69ba4487a1a4390d3e4b083f1734c92c1358f242
Mahdi <mahdivafaii1999@gmail.com>
2023-05-25 21:32:56 +0330

    Fix minor issues of calendar design

--- src/assets/Icons.jsx
+++ src/assets/Icons.jsx
@@ -61,16 +61,16 @@ const icons = {
         cy="11.5586"
         r="7.06194"
         stroke="#323232"
-        stroke-width="1.5"
-        stroke-linecap="round"
-        stroke-linejoin="round"
+        strokeWidth="1.5"
+        strokeLinecap="round"
+        strokeLinejoin="round"
       />
       <path
         d="M20.0033 20.5033L16.0517 16.5517"
         stroke="#323232"
-        stroke-width="1.5"
-        stroke-linecap="round"
-        stroke-linejoin="round"
+        strokeWidth="1.5"
+        strokeLinecap="round"
+        strokeLinejoin="round"
       />
     </svg>
   ),
@@ -85,9 +85,9 @@ const icons = {
       <path
         d="M14 8.00009L10 12.0001L14 16.0001"
         stroke="#7D828C"
-        stroke-width="1.5"
-        stroke-linecap="round"
-        stroke-linejoin="round"
+        strokeWidth="1.5"
+        strokeLinecap="round"
+        strokeLinejoin="round"
       />
     </svg>
   ),
@@ -102,9 +102,42 @@ const icons = {
       <path
         d="M10 16.0001L14 12.0001L10 8.00009"
         stroke="#7D828C"
-        stroke-width="1.5"
-        stroke-linecap="round"
-        stroke-linejoin="round"
+        strokeWidth="1.5"
+        strokeLinecap="round"
+        strokeLinejoin="round"
+      />
+    </svg>
+  ),
+  addIcon: (
+    <svg
+      width="18"
+      height="18"
+      viewBox="0 0 18 18"
+      fill="none"
+      xmlns="http://www.w3.org/2000/svg"
+    >
+      <path
+        fillRule="evenodd"
+        clipRule="evenodd"
+        d="M15 17H3C1.895 17 1 16.105 1 15V3C1 1.895 1.895 1 3 1H15C16.105 1 17 1.895 17 3V15C17 16.105 16.105 17 15 17Z"
+        stroke="white"
+        strokeWidth="1.5"
+        strokeLinecap="round"
+        strokeLinejoin="round"
+      />
+      <path
+        d="M9 5V13"
+        stroke="white"
+        strokeWidth="1.5"
+        strokeLinecap="round"
+        strokeLinejoin="round"
+      />
+      <path
+        d="M13 9H5"
+        stroke="white"
+        strokeWidth="1.5"
+        strokeLinecap="round"
+        strokeLinejoin="round"
       />
     </svg>
   ),
