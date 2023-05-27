2813a0a5be76844bb6f98f2d0b33897a672a0b79
Rosehajizadeh <rose_kr_2020@yahoo.com>
2023-05-27 22:30:52 +0330

    edit home page

--- src/pages/home.jsx
+++ src/pages/home.jsx
@@ -1,23 +1,4 @@
-import { ProjectCard } from "../components/ProjectCard/ProjectCard";
-
-const projectItems = {
-  projectTitle: "پروژه اول",
-  taskTitle: "این یک تیتر برای این تسک است.",
-  date: "۵ مهر - فردا",
-  time: "۲ / ۱۲",
-  id: 1,
-  tags: [
-    {
-      id: 1,
-      tagTitle: "درس",
-      tagColor: "#BFFDE3",
-    },
-    { id: 2, tagTitle: "پروژه", tagColor: "#EEDFF7" },
-  ],
-  userName: "NA",
-};
-
 const Home = () => {
-  return <ProjectCard {...projectItems}></ProjectCard>;
+  return <div>Home Page</div>;
 };
 export default Home;
