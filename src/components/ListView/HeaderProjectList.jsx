55a1720a8e2cd33479f4b4d80697aca7a748cf5f
tajikmohsen <tajikmohsen@outlook.com>
2023-05-29 22:27:47 +0330

    Edit button filter

--- src/components/ListView/HeaderProjectList.jsx
+++ src/components/ListView/HeaderProjectList.jsx
@@ -10,10 +10,10 @@ const HeaderProjectList = () => {
         </span>
       </div>
       <span className="w-1 h-[22px] border-l-[1px] border-[#999999] mr-28 ml-10" />
-      <div className="flex gap-1 items-center">
+      <button className="flex gap-1 items-center">
         <span>{icons.FilterIcon}</span>
         <span className="text-black font-medium text-xs">فیلترها</span>
-      </div>
+      </button>
       <span className="bg-[#E9F9FF] text-black font-medium text-xs mr-5 h-[26px] p-1 flex items-center">
         دسته‌بندی‌شده با: وضعیت
       </span>
