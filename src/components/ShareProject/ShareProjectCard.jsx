350ac914cc968a69d9c1280a833377c9305e8732
Rosehajizadeh <rose_kr_2020@yahoo.com>
2023-06-04 12:53:48 +0330

    add share project to main layout

--- src/components/ShareProject/ShareProjectCard.jsx
+++ src/components/ShareProject/ShareProjectCard.jsx
@@ -5,6 +5,8 @@ import profile from "../../assets/images/profile.png";
 import Card from "../Card/Card";
 import icons from "../../assets/Icons";
 import { SharedUser } from "./SharedUser";
+import { Dialog, Transition } from "@headlessui/react";
+import { Fragment } from "react";
 
 const ShareProjectCard = ({ openModal, setOpenModal }) => {
   const info = [
@@ -27,40 +29,76 @@ const ShareProjectCard = ({ openModal, setOpenModal }) => {
   };
 
   return (
-    <Card
-      className={
-        openModal
-          ? "max-w-[470px] w-full rounded-xl  p-5 bg-white flex flex-col"
-          : "hidden"
-      }
-    >
-      <div className="flex items-center pb-[45px]">
-        <div className="cursor-pointer" onClick={handleClose}>
-          {icons.CloseIcon}
-        </div>
-        <div className="w-full text-center font-semibold">
-          به اشتراک‌گذاری پروژه‌
-        </div>
-      </div>
-      <ProjectInvitationForm />
-      <CopyLink className="mb-[29px]" />
-      <span className="text-sm text-[#7d828c] block mb-5">
-        اشتراک‌ گذاشته شده با
-      </span>
-      <div className="flex  flex-col gap-5">
-        {info.map((user) => (
-          <div className="flex justify-between " key={user.id}>
-            <SharedUser
-              name={user.name}
-              email={user.email}
-              image={user.image}
-              isOwner={user.isOwner}
-            />
-            <PermissionTitle isOwner={user.isOwner} />
+    <>
+      <Transition appear show={openModal} as={Fragment}>
+        <Dialog as="div" className="relative z-10" onClose={handleClose}>
+          <Transition.Child
+            as={Fragment}
+            enter="ease-out duration-300"
+            enterFrom="opacity-0"
+            enterTo="opacity-100"
+            leave="ease-in duration-200"
+            leaveFrom="opacity-100"
+            leaveTo="opacity-0"
+          >
+            <div className="fixed inset-0 bg-black bg-opacity-25" />
+          </Transition.Child>
+
+          <div className="fixed inset-0 overflow-y-auto">
+            <div className="flex min-h-full items-center justify-center p-4">
+              <Transition.Child
+                as={Fragment}
+                enter="ease-out duration-300"
+                enterFrom="opacity-0 scale-95"
+                enterTo="opacity-100 scale-100"
+                leave="ease-in duration-200"
+                leaveFrom="opacity-100 scale-100"
+                leaveTo="opacity-0 scale-95"
+              >
+                <Dialog.Panel>
+                  <Card
+                    className={
+                      openModal
+                        ? "w-[470px] rounded-xl  p-5 bg-white flex flex-col"
+                        : "hidden"
+                    }
+                  >
+                    <Dialog.Title>
+                      <div className="flex items-center pb-[45px]">
+                        <div className="cursor-pointer" onClick={handleClose}>
+                          {icons.CloseIcon}
+                        </div>
+                        <div className="w-full text-center font-semibold">
+                          به اشتراک‌گذاری پروژه‌
+                        </div>
+                      </div>
+                    </Dialog.Title>
+                    <ProjectInvitationForm />
+                    <CopyLink className="mb-[29px]" />
+                    <span className="text-sm text-[#7d828c] block mb-5">
+                      اشتراک‌ گذاشته شده با
+                    </span>
+                    <div className="flex  flex-col gap-5">
+                      {info.map((user) => (
+                        <div className="flex justify-between " key={user.id}>
+                          <SharedUser
+                            name={user.name}
+                            email={user.email}
+                            image={user.image}
+                            isOwner={user.isOwner}
+                          />
+                          <PermissionTitle isOwner={user.isOwner} />
+                        </div>
+                      ))}
+                    </div>
+                  </Card>
+                </Dialog.Panel>
+              </Transition.Child>
+            </div>
           </div>
-        ))}
-      </div>
-    </Card>
+        </Dialog>
+      </Transition>
+    </>
   );
 };
 
