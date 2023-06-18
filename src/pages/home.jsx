82dddea20eba3f6c8f9e84dd618cc5702ba3145e
nima doustdar <nimadoustdar99@gmail.com>
2023-06-18 15:46:26 +0330

    remove from column view and add in home for sample

--- src/pages/home.jsx
+++ src/pages/home.jsx
@@ -1,4 +1,20 @@
-const Home = () => {
-  return <div>Home Pgae</div>
-};
+import { useState } from "react";
+import CreateNewBoardTitle from "../components/BoardTitle/CreateNewBoardTitle";
+
+function Home() {
+  const [openNewBoardModal, setOpenNewBoardModal] = useState(false);
+  const addNewBoardTitleClickHandler = () => {
+    setOpenNewBoardModal(true);
+  };
+  return (
+    <div>
+      <button onClick={addNewBoardTitleClickHandler}>کلیک کنید</button>
+      <CreateNewBoardTitle
+        setOpenNewBoardModal={setOpenNewBoardModal}
+        openNewBoardModal={openNewBoardModal}
+      />
+    </div>
+  );
+}
+
 export default Home;
