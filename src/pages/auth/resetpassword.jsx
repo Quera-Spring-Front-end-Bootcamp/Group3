981d7d660c346fb82f9da26423e4c6277dcb732f
Rosehajizadeh <rose_kr_2020@yahoo.com>
2023-06-26 19:56:15 +0330

    fix react hook form validation

--- src/pages/auth/resetpassword.jsx
+++ src/pages/auth/resetpassword.jsx
@@ -3,7 +3,7 @@ import Card from "../../components/Card/Card";
 import Input from "../../components/Input";
 import Button from "../../components/Button";
 import toast from "react-hot-toast";
-import { useNavigate,useLocation } from "react-router-dom";
+import { useNavigate, useLocation } from "react-router-dom";
 import AXIOS from "../../Utils/axios";
 
 //  لینک ایمیل شده جهت تست به فرمت زیر تغییر یابد و توکن جدید جایگزین گردد
@@ -14,35 +14,28 @@ const ResetPassword = () => {
     register,
     formState: { errors },
     handleSubmit,
+    watch,
   } = useForm();
-  const query = new URLSearchParams(useLocation().search)
+  const query = new URLSearchParams(useLocation().search);
   const navigate = useNavigate();
 
   async function onSubmit(data) {
-
-    if (data.password === data.confirmPassword) {
-      try {
-        await AXIOS.post("/auth/reset-password", {
-          token: query.get("token"),
-          password: data.password,
-        });
-        toast.success("عملیات با موفقیت انجام شد.");
-        navigate("/main/listView");
+    try {
+      await AXIOS.post("/auth/reset-password", {
+        token: query.get("token"),
+        password: data.password,
+      });
+      toast.success("عملیات با موفقیت انجام شد.");
+      navigate("/main/listView");
     } catch (e) {
-        toast.error("عملیات با خطا مواجه شد.");
-      }
-
-    }else {
-      toast.error("داده های وارده یکسان نیستند")
+      toast.error("عملیات با خطا مواجه شد.");
     }
-
   }
 
   return (
     <div className="flex flex-row items-center justify-center w-screen h-screen">
       <Card title="عملیات بازنشانی رمز">
         <form className="w-full mt-7" onSubmit={handleSubmit(onSubmit)}>
-
           <div className="mt-4">
             <Input
               type="password"
@@ -51,11 +44,6 @@ const ResetPassword = () => {
               error={errors.password}
               register={register("password", {
                 required: "این فیلد الزامی می باشد!",
-                // pattern: {
-                //   value: /(?=.*\d)(?=.*[a-z])(?=.*[A-Z]).{8,}/g,
-                //   message:
-                //     "پسورد باید حداقل یک عدد و یک حرف بزرگ و کوچک و حداقل 8 کاراکتر یا بیشتر داشته باشد.",
-                // },
               })}
             />
           </div>
@@ -64,14 +52,12 @@ const ResetPassword = () => {
               type="Password"
               label="تایید رمز عبور"
               id="confirmPassword"
-              error={errors.password}
+              error={errors.confirmPassword}
               register={register("confirmPassword", {
                 required: "این فیلد الزامی می باشد!",
-                // pattern: {
-                //   value: /(?=.*\d)(?=.*[a-z])(?=.*[A-Z]).{8,}/g,
-                //   message:
-                //     "پسورد باید حداقل یک عدد و یک حرف بزرگ و کوچک و حداقل 8 کاراکتر یا بیشتر داشته باشد.",
-                // },
+                validate: (value) =>
+                  value === watch("password") ||
+                  "تکرار گذرواژه جدید با گذرواژه جدید مطابقت ندارد",
               })}
             />
           </div>
