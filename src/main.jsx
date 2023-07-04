affe7ee2b24b45cc715ab1c7b1958ab09875e5a5
flamma <mahdivafaii1999@gmail.com>
2023-07-04 19:17:12 +0330

    exit strict mode - required for dragging boards

--- src/main.jsx
+++ src/main.jsx
@@ -8,11 +8,11 @@ import { Provider } from "react-redux";
 import store from "./redux/store";
 
 ReactDOM.createRoot(document.getElementById("root")).render(
-  <React.StrictMode>
-    <Provider store={store}>
-      <BrowserRouter>
-        <App />
-      </BrowserRouter>
-    </Provider>
-  </React.StrictMode>
+  // <React.StrictMode>
+  <Provider store={store}>
+    <BrowserRouter>
+      <App />
+    </BrowserRouter>
+  </Provider>
+  // </React.StrictMode>
 );
